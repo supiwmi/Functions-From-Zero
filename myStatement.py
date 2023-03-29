ee863fa1cfcda6da3d361daeff98b724d9594b52
supiwmi <35697929+supiwmi@users.noreply.github.com>
2023-03-29 13:14:16 +0000

    add file to repo

---
+++ myStatement.py
@@ -0,0 +1,11 @@
+my_list = ["Apple", "Cherry", "Lemon", "Banana"]
+my_dict = {"drink": "coffee", "milk": "whole"}
+
+for item in my_list:
+    print(f"My favorite thing to eat is: {item}")
+
+for key, value in my_dict.items():
+    print(f"My favorite thing to drint is: {value}")
+
+#my_dict = my_dict
+#my_list2 =
